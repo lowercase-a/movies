9df548bb715b30d2ab71e38c646753dc13fc7848
Brian <riceb53@gmail.com>
2019-03-03 10:22:43 -0600

    add create action

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,12 +1,24 @@
 class Api::MoviesController < ApplicationController
   def index
+    # show all the movies from the database
     @movies = Movie.all
     render 'index.json.jbuilder'
   end
 
   def show
+    # show one particular movie from the database
     the_id = params[:id]
     @movie = Movie.find_by(id: the_id)
     render 'show.json.jbuilder'
+  end
+
+  def create
+    # make a new movie in the database
+    @movie = Movie.new(
+      title: params[:input_title],
+      runtime: params[:input_runtime]
+    )
+    @movie.save
+    render 'create.json.jbuilder'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     get '/movies' => 'movies#index'
     get '/movies/:id' => 'movies#show'
+    post '/movies' => 'movies#create'
   end
 end
